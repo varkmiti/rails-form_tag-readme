8178587d8e9b4f3579a4581aaad245b0ef5452de
Mark Viti <+>
2022-06-24 11:48:46 -0400

    commit tree 0d7ae9271ce52a65df8f1ec99cd403ed2d3e8da8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+  end
   
 end
